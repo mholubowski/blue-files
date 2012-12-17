2d5de883090939bb8f0bc779637ef879d50d4fbf
Mike Holubowski <mike.holubowski@gmail.com>
2012-12-17 15:05:28 -0800

    First commit..added README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlueFiles::Application.config.secret_token = '6df1f7ff5c704f9988b47bee93b98d3cdc039a37471abda88d9f10f74ee0a2e7aa701b80de9d4b39232e8dd8fe146f91a33fcd1897033c0a0b797a8037ea71ce'
