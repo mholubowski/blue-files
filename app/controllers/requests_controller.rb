9c1b3fcaf86342730612d8f01278079c04bd462d
Mike Holubowski <mike.holubowski@gmail.com>
2012-12-31 19:06:06 -0800

    Recent requests and create form working (no ajax) on index

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,7 @@
 class RequestsController < ApplicationController
 
 	def index
-		 @recent_requests = current_account.requests.all(limit: 10)
+		 @recent_requests = current_account.requests.all(limit: 5)
 	end
 
 	def show
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+  	@request = current_account.requests.build if signed_in?
+  	@recent_requests = current_account.requests.all(limit: 5)
+
   end
 end
