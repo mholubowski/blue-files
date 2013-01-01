0849a6df27878e23d7634f268a64df066759d768
Mike Holubowski <mike.holubowski@gmail.com>
2012-12-31 19:25:02 -0800

    IMPORTANT added 'if signed_in?' to welcome controller to fix heroku

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,10 @@
 class WelcomeController < ApplicationController
   def index
   	@request = current_account.requests.build if signed_in?
-  	@recent_requests = current_account.requests.all(limit: 5)
+  	@recent_requests = current_account.requests.all(limit: 5) if signed_in?
 
   	@recent_documents = current_account.documents.all(
   						order: 'created_at DESC', 
-  						limit:  5)
+  						limit:  5) if signed_in?
   end
 end
