c45ac988af54524afaf5b8193a9f534fc42a9cba
Mike Holubowski <mike.holubowski@gmail.com>
2012-12-31 19:19:55 -0800

    Recent documents stream on index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,8 @@ class WelcomeController < ApplicationController
   	@request = current_account.requests.build if signed_in?
   	@recent_requests = current_account.requests.all(limit: 5)
 
+  	@recent_documents = current_account.documents.all(
+  						order: 'created_at DESC', 
+  						limit:  5)
   end
 end
