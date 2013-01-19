38b8937fc834d8417f070a9fda0ead992e92491e
Mike Holubowski <mike.holubowski@gmail.com>
2013-01-19 13:52:39 -0800

    response and errors working

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -9,34 +9,28 @@ jQuery(function() {
 
 subscription = {
   setupForm: function() {
-                                                              console.log("step 2");
     return $('#new_subscription').submit(function() {
       $('input[type=submit]').attr('disabled', true);
-                                                              console.log("step 3");
       subscription.processCard();
-                                                              console.log("step 4");
       return false;
     });
   },
   processCard: function() {
     var card;
-                                                              console.log("step 5");
     card = {
       number:   $('#card_number').val(),
       cvc:      $('#card_code').val(),
       expMonth: $('#card_month').val(),
       expYear:  $('#card_year').val()
     };
-                                                              console.log("step 6");
     return Stripe.createToken(card, subscription.handleStripeResponse);
   },
   handleStripeResponse: function(status, response) {
     if (status === 200) {
-                                                              console.log("step 7");
       return alert(response.id);
     } else {
-                                                              console.log("step 8");
-      return alert(response.error.message);
+      $('#stripe_error').text(response.error.message);
+      $('input[type=submit]').attr('disabled', false);
     }
   }
 };
