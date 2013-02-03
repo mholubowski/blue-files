915752363881461b68698468b78808fb920322cc
Mike Holubowski <mike.holubowski@gmail.com>
2013-02-03 13:15:06 -0800

    Password reset funcitonality working/ styling email

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -16,11 +16,13 @@ class PasswordResetsController < ApplicationController
 	def update
 		@Account = Account.find_by_password_reset_token!(params[:id])
 		if @Account.password_reset_sent_at < 2.hours.ago
-			redirect_to new_password_reset_path, :alert => "Password &crarr; 
-			reset has expired, please request again."
-		elsif @Account.update_attributes(params[:Account])
-			redirect_to root_url, :notice => "Password has been reset."
+			flash[:error] = "Password reset has expired, please request again."
+			redirect_to new_password_reset_path 
+		elsif @Account.update_attributes(params[:account])
+			flash[:notice] = "Password has been reset."
+			redirect_to root_url
 		else
+			flash.now[:notice] = "Else"
 			render :edit
 		end
 	end
